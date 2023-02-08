0be6a6e85d763b0119dd4196b50b08938f577e48
Moose17 <63780114+Mooose17@users.noreply.github.com>
2023-02-08 20:07:30 +0000

    added pages for all and specific blog posts

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,51 @@
 import React from "react";
-import Header from "@/components/header";
-import Navbar from "@/components/Navbar";
+import fs from "fs";
+import matter from "gray-matter";
+import Image from "next/image";
+import Link from "next/link";
+import Style from "../styles/card.module.css";
 
-const index = () => {
+const index = ({ posts }) => {
   return (
-    <div>
-      <Header />
-      <Navbar />
+    <div className={Style.container}>
+      {posts.map(({ slug, frontmatter }) => (
+        <div key={slug}>
+          <Link href={`/post/${slug}`} className={Style.link}>
+            <Image
+              className={Style.image}
+              width={650}
+              height={340}
+              alt={frontmatter.title}
+              src={`/${frontmatter.socialImage}`}
+            />
+            <h3 className={Style.title}>
+              {frontmatter.title} - {frontmatter.date}
+            </h3>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default index;
+
+export async function getStaticProps() {
+  const files = fs.readdirSync("posts");
+  const posts = files.map((fileName) => {
+    const slug = fileName.replace(".md", "");
+    const readFile = fs.readFileSync(`posts/${fileName}`, "utf-8");
+    const { data: frontmatter } = matter(readFile);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
--- pages/music.js
+++ pages/music.js
@@ -1,14 +1,58 @@
 import React from "react";
-import Header from "@/components/header";
-import Navbar from "@/components/Navbar";
+import fs from "fs";
+import matter from "gray-matter";
+import Image from "next/image";
+import Link from "next/link";
+import Style from "../styles/card.module.css";
 
-const music = () => {
+const music = ({ posts }) => {
   return (
-    <div>
-      <Header />
-      <Navbar />
+    <div className={Style.container}>
+      {posts.map(({ slug, frontmatter }) => (
+        <div key={slug}>
+          <Link href={`/post/${slug}`} className={Style.link}>
+            <Image
+              className={Style.image}
+              width={650}
+              height={340}
+              alt={frontmatter.title}
+              src={`/${frontmatter.socialImage}`}
+            />
+            <h3 className={Style.title}>
+              {frontmatter.title} - {frontmatter.date}
+            </h3>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default music;
+
+export async function getStaticProps() {
+  const files = fs.readdirSync("posts");
+  const posts = files
+    .map((fileName) => {
+      const slug = fileName.replace(".md", "");
+      const readFile = fs.readFileSync(`posts/${fileName}`, "utf-8");
+      const { data: frontmatter } = matter(readFile);
+
+      if (frontmatter.tags === "music") {
+        return {
+          slug,
+          frontmatter,
+        };
+      }
+      return null;
+    })
+    .filter((post) => {
+      return !!post;
+    });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
